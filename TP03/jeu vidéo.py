34827f2a41b2e9c1a4a2009b0ce60f783d3cb06c
Aaz0og <aazoog911@gmail.com>
2021-10-03 20:54:23 +0200

    Ajout TP03

---
+++ TP03/jeu vidéo.py
@@ -0,0 +1,19 @@
+class JeuVideo:
+    def __init__(self, titre, plateformes, multijoueur):
+        self.adefinir = ...
+        self.adefinir = ...
+        self.adefinir = ...
+
+    def addPlateforme(self, ...):
+        if not(... in self.adefinir):
+            self.adefinir.append(...)
+
+    def removePlateforme(self, ...):
+        if ... in self.adefinir:
+            self.adefinir.remove(...)
+
+    def isOnPlateforme(self, ...):
+        return ... in self.adefinir
+
+    def isMultijoueur(self):
+        return self.adefinir
