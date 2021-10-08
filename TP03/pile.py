dd965829bc2fddd13bc5fa7a7a2c2925703e52d2
Aaz0og <91578277+Aaz0og@users.noreply.github.com>
2021-10-08 08:26:30 +0200

    Update pile.py

--- TP03/pile.py
+++ TP03/pile.py
@@ -6,34 +6,64 @@ class Cellule:
         self.valeur = valeur
         self.suivant = suivant
 
-
 class Pile:
     def __init__(self, sommet=0, taille=0):
         self.sommet = Cellule(sommet, None)
         self.taille = taille
 
     def estVide(self):
+        """
+        Regarde la taille de la file pour voir si elle est vide
+
+        Returns:
+            bool: True si vide False si non vide
+        """
         if self.taille == 0:
             return True
         else:
             return False
 
     def valeurSommet(self):
+        """
+        Regarde la valeur du sommet
+
+        Returns:
+            int: Valeur du sommet
+        """
         return self.sommet.valeur
 
     def empiler(self, ajout):
+        """
+        Ajoute le numéro voulu au dessus du numéro actuel
+
+        Args:
+            ajout (int): nombre que l'on souhaite ajouter a la pile
+        """
         self.sommet = Cellule(ajout, self.sommet)
         self.taille += 1
 
     def depiler(self):
+        """
+        Retire le sommet de la pile
+
+        Returns:
+            int: Sommet de la pile qui a été retiré
+        """
         y = self.sommet
         self.sommet = self.sommet.suivant
         self.taille -= 1
         return y
 
     def __len__(self):
+        """
+        Sers a configurer la commande len
+
+        Returns:
+            int: Taille de la file
+        """
         return self.taille
 
+#! Tests
 
 pile_test = Pile()
 print(len(pile_test))
