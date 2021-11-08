1cae71466f62287351e7eabac915f2c547bd911b
Aaz0og <aazoog911@gmail.com>
2021-11-08 19:36:51 +0100

    Update turtle test.py

--- PROJET/turtle test.py
+++ PROJET/turtle test.py
@@ -1,15 +1,18 @@
 import turtle
 import random
 import time
-wn = turtle.Screen()
-wn.bgcolor("black")
-wn.title("Balle")
-wn.tracer(0)
+sc = turtle.Screen()
+sc.bgcolor("black")
+sc.title("Balle Niels (Mehdi)")
+sc.tracer(0)
 balls = []
 for _ in range(1):
     balls.append(turtle.Turtle())
-
-
+"""
+Je travaille tout seul je n'ai pas eu le temps de travailler ces vacances, Mehdi n'a rien fais il m'a contacté samedi avec une excuse pour ne pas travailler
+Et même pas une ligne de code a donner. je voulais vous en parler aujoud'hui mais je n'étais pas la. J'ai tout fais sur turtle mais je le passerais sur tk
+c'est juste plus rapide avec turtle. Bonne soirée
+"""
 couleurs = ["red", "blue", "yellow", "purple", "orange", "white"]
 for ball in balls:
     ball.shape("circle")
@@ -23,24 +26,20 @@ for ball in balls:
     ball.dy = -2
     ball.dx = random.randint(-3, 3)
 while True:
-    
+    # Boucle True pour le répeter jusqu'à la fin pour l'instant je n'ai pas de meileure technique
     for ball in balls:
-        wn.update()
+        sc.update()
         ball.dy -= g
         ball.sety(ball.ycor()+ball.dy)
         ball.setx(ball.xcor()+ball.dx)
-        # mur droite
+        # Toujours pareil mais pour le mur de droite
         if ball.xcor() > 300:
             ball.dx *= -1
-        # mur gauche
+        # Pareil que pour le bas mais pour le mur de gauche
         if ball.xcor() < -300:
             ball.dx *= -1
-        # Regarde si doit rebondir sur sol
-        
+        # Regarde les coordonnées y de la balle et si elle atteinds quelque chose en dessous de -300 (le bas de l'écran) on inverse la vitesse y pour que la balle remonte
         if ball.ycor() < -300:
             ball.dy *= -1
-            print(ball.dy)
 
-
-
-wn.mainloop()
+sc.mainloop()
