dd9ddeffb766faa3c188b29d7e8ed8cb9ee346d5
Aaz0og <91578277+Aaz0og@users.noreply.github.com>
2021-11-12 09:16:35 +0100

    Cours 12 nov 2021

---
+++ PROJET/CARLON-MISMER_Niels_08-11 corrige.py
@@ -0,0 +1,54 @@
+""" Il faut tâcher de vous rapprocher du cahier des charges pour la version finale du projet:
+    - la programmation objet est nécessaire; vous devez vous appuyer sur le travail d'analyse fonctionnelle produit en classe
+    - aucune animation n'est attendue : revenir à Tkinter ne vous fera pas perdre de temps, mais vous permettra de rentrer dans les attentes de ce travail
+    - on peut se contenter de l'affichage de l'affichage de quelques positions successives de la bulle, illustrant le bon fonctionnement de chacun des rebonds
+Je discute avec Mehdi ce matin.
+J'espère vous voir également.
+"""
+
+
+import turtle
+import random
+import time
+sc = turtle.Screen()
+sc.bgcolor("black")
+sc.title("Balle Niels (Mehdi)")
+sc.tracer(0)
+balls = []
+for _ in range(1):
+    balls.append(turtle.Turtle())
+"""
+Je travaille tout seul je n'ai pas eu le temps de travailler ces vacances, Mehdi n'a rien fais il m'a contacté samedi avec une excuse pour ne pas travailler
+Et même pas une ligne de code a donner. je voulais vous en parler aujoud'hui mais je n'étais pas la. J'ai tout fais sur turtle mais je le passerais sur tk
+c'est juste plus rapide avec turtle. Bonne soirée
+"""
+couleurs = ["red", "blue", "yellow", "purple", "orange", "white"]
+for ball in balls:
+    ball.shape("circle")
+    ball.color(random.choice(couleurs))
+    ball.penup()
+    ball.speed(0)
+    x = random.randint(-290, 290)
+    y = random.randint(200, 400)
+    ball.goto(x, y)
+    g = 0.1
+    ball.dy = -2
+    ball.dx = random.randint(-3, 3)
+while True:
+    # Boucle True pour le répeter jusqu'à la fin pour l'instant je n'ai pas de meileure technique
+    for ball in balls:
+        sc.update()
+        ball.dy -= g
+        ball.sety(ball.ycor()+ball.dy)
+        ball.setx(ball.xcor()+ball.dx)
+        # Toujours pareil mais pour le mur de droite
+        if ball.xcor() > 300:
+            ball.dx *= -1
+        # Pareil que pour le bas mais pour le mur de gauche
+        if ball.xcor() < -300:
+            ball.dx *= -1
+        # Regarde les coordonnées y de la balle et si elle atteinds quelque chose en dessous de -300 (le bas de l'écran) on inverse la vitesse y pour que la balle remonte
+        if ball.ycor() < -300:
+            ball.dy *= -1
+
+sc.mainloop()
---
+++ TP06/ImplementationComplexite.py
@@ -0,0 +1,84 @@
+import matplotlib.pyplot as plt  # Pour faire des graphiques
+import random  # Pour faire de l'aléatoire
+import time  # Pour manipuler le temps
+import sys
+# partie I - Recherche d'un minimum
+def alea(j):
+    '''In : i un entier, la taille de la liste à générer
+    Out : une liste de i entiers aléatoires compris entre 0 et 100'''
+    liste_aleatoire = []
+    for _ in range(j+1):
+        n = random.randint(0,100)
+        liste_aleatoire.append(n)
+    print("Liste aléatoire:",liste_aleatoire)
+    return liste_aleatoire
+
+# def Minimum(L):
+""" In : L est une liste de nombres non vide
+    Out : Le minimum de L"""
+
+# Fonctions nécessaires au test de la complexité des algorithmes, utilisées dans toutes les parties
+
+
+def TempsAlgo(i, fonction):
+    '''In : i un entier, la taille du tableau
+    Out : le temps mis par l'algorithme'''
+    A = alea(i)  # On fait un tableau rempli de i valeurs aléatoires
+    debut = time.time_ns()  # donne l'heure en nanoseconde.
+    fonction(A)
+    return time.time_ns()-debut
+
+
+def trace(fonction, imax):
+    """ In : fonction est le nom d'une fonction qui agit sur une liste et 
+    dont on veut afficher la durée de calcul en fonction de la longueur de la liste
+    Out : Affichage du temps en fonction de la longueur de la liste
+    Des listes de longueur 0 à imax sont testées
+    Il est possible d'enregistrer le graphique"""
+    temps = []
+    taille = [i for i in range(1, imax+1)]
+    for t in taille:
+        temps += [TempsAlgo(t, fonction)]
+    plt.title("temps de calcul en fonction de la longueur de la liste")
+    # On trace la figure les tailles en abscisse,les temps en ordonnées
+    plt.plot(taille, temps)
+    plt.xlabel('Taille liste')
+    plt.ylabel('Temps(ns)')
+    plt.show()  # Affichage
+    # fig.savefig('graph.png') # si on veut sauvegarder la figure (ATTENTION de changer le nom du fichier pour ne pas écraser les précédents)
+
+# II - algos de tri
+# A - Tri par sélection
+def TriSelection(A):
+    """In : liste de nombres A
+    Out : liste triée par sélection"""
+    for i in range(len(A)):
+        # Trouve le minimum
+        idx_min = i
+        for o in range(i+1, len(A)):
+            if A[idx_min] > A[o]:
+                idx_min = o
+        # Change le minimum trouvé avec le premier nombre
+        A[i], A[idx_min] = A[idx_min], A[i]
+    return A
+
+A= alea(10)
+print("Après Tri:", TriSelection(A))
+
+
+
+"""   
+# B - Tri par insertion
+#def TriInsertion(A):
+    ''' In : liste de nombres A
+    Out : liste triée par insertion'''    
+#def Inserer(i,A):
+    ''' In : liste de nombres A, 
+    i indice de la valeur à insérer dans la partie de A qui la précède
+    Out : A est triée jusqu'à l'indice i inclus.'''
+"""
+
+
+
+
+
