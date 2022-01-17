cbd6803115f0b368229a9d4ffa3399f3e06d8fe9
Aaz0og <aazoog911@gmail.com>
2022-01-17 14:10:13 +0100

    Exercice

--- PROJETS/PROJET V2/Algorithmes_LAaliyah_CLeo.py
+++ PROJETS/PROJET V2/Algorithmes_LAaliyah_CLeo.py
@@ -27,34 +27,48 @@ le compteur représente le nombre de carrés"""
 
 
 def algo2(x,y):
+    """
+    Trouve le nombre de carres dans deux listes pour chaque absisse
+
+    Args:
+        x (list): liste des cordonnees en x
+        y (lsit): liste des coordonnees en y
+
+    Returns:
+        int: Nombre de carres
+    """
     liste_x = []
     liste_y = []
-    cpt = 0
+    resul = 0
     '''Crée une liste qui contient toutes les valeurs possibles pour des longueurs.'''
     for i in range(len(x)):
         for j in range(len(x)):
             if x[j] - x[i] > 0:
                 liste_x.append(x[j] - x[i])
+                print("liste_x:",liste_x)
 
     '''Crée une liste qui contient toutes les valeurs possibles pour des largeurs.'''
     for i in range(len(y)):
         for j in range(len(y)):
             if y[j] - y[i] > 0:
                 liste_y.append(y[j] - y[i])
+                print("liste_y:", liste_y)
 
-    '''Compare les deux listes pour savoir combien il y a de valeurs identiques.'''
+    '''Ici on compare les 2 listes et si les valeurs sont identiques on ajoute 1 au compteur résul'''
     for i in range(len(liste_x)):
         for j in range(len(liste_y)):
             if liste_x[i] == liste_y[j]:
-                cpt += 1
-    return cpt
+                resul += 1
+                
+    return resul
 
-x = [ 0,  3, 6, 9]
-y = [ 0,  3, 6, 9]
+x = [ 0, 2, 5, 10]
+y = [ 0, 3, 5]
 print(algo2(x,y))
+
 for i in range(9):
     pass
-    """Complexité de l'algo2:
+"""Complexité de l'algo2:
 On prend n la longueur de la liste
 O(n*log(n))
 """
